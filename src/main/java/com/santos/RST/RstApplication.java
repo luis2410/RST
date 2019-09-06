cee2ac852c1d48d08d1d77ece754cd4846427a48
Unknown <ing.fsantosn@gmail.com>
2019-09-05 21:31:28 -0500

    wrer

---
+++ src/main/java/com/santos/RST/RstApplication.java
@@ -0,0 +1,13 @@
+package com.santos.RST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RstApplication.class, args);
+	}
+
+}
